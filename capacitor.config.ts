4be0fb839b8dc4bc9cc4a6e8e446fb5d8f3dbcaf
LAPTOP-L3RI7VV1\ADRIAN99_MM <adrian99_@outlook.com>
2021-07-28 19:32:42 -0500

    Mi primer commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'hotel',
-  webDir: 'dist',
+  webDir: 'dist/HOTEL',
   bundledWebRuntime: false
 };
 
